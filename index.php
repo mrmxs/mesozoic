e1c745adf8764e6d030e24f8a4c87a9f08703407
mrmxs <maryzmaxs@gmail.com>
2016-07-01 00:37:17 +0300

    Add 1-st page: 'Feed'. 5 news gens in circle. (Same text)

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mesozoic | Feed</title>
+    </head>
+    <body>
+        <?php
+        
+        $source_date = "30/06/2016";
+        $source_title = "Midi-контроллер как аллювий";
+        $source_text = "Конечно, нельзя не принять во внимание тот факт, что уравнение Эйлера представляет собой трансцендентальный псевдомицелий. Солифлюкция косвенно трансформирует угол крена. Аксиома силлогизма трудна в описании. Надо сказать, что дуализм нетривиален. Кластерное вибрато поразительно. Капилляр контролирует устойчивый волчок. Как уже указывалось, ортштейн дает экваториальный момент. Арпеджированная фактура, в силу третьего закона Ньютона, возможна. Как было показано выше, драм-машина индуктивно иссушает интеллигибельный нонаккорд.";
+        
+for ($index = 0; $index < 6; $index++) { 
+    
+    $date = $source_date;
+    $title = $source_title;
+    $text = $source_text;
+    
+    
+    echo "<div class='post'>
+  <h6 class='date'>$date</h6>
+  <h1 class='title'>$title</h1>
+  <div class='text'>$text</div>
+  <br>
+  <hr/>
+</div>
+";    
+} 
+        ?>
+    </body>
+</html>
+
+<!--
+//https://yandex.ru/referats/
+-->
