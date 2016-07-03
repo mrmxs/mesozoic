e98686579829acc328f89aa0bf0e8f893411d9de
mrmxs <maryzmaxs@gmail.com>
2016-07-03 14:26:27 +0300

    Добавлен вход в админ-панель по ключю .htpasswd

--- views/header.php
+++ views/header.php
@@ -3,5 +3,6 @@
                 <a class="navbar-brand" href="/Mesozoic/">
                     <span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> 
                     Mesozoic Blog</a>
+                <a href="./admin/" class="pull-right btn btn-primary admin-center" role="button">Панель администратора</a>
             </div>
         </header>
